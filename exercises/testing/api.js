8a395c19ef07ed3950a869f52854fa2c31e3efc6
Nikolay <likurg42@gmail.com>
2020-11-16 18:59:48 +0300

    solve testing exercise

--- exercises/testing/api.js
+++ exercises/testing/api.js
@@ -5,20 +5,38 @@ const users = require('./users')
 const app = express()
 
 app.use(morgan('dev'))
-app.use(urlencoded({extended: true}))
+app.use(urlencoded({ extended: true }))
 app.use(json())
 
+app.get('/', async (req, res) => {
+    res.status(200);
+    res.write('Hello');
+    res.end();
+})
+
 app.get('/user/:id', async (req, res) => {
-  const id = req.id
-  // should ge user by given id in route param
-  const user = await users.findUser(user => user.id === id)
-  res.status(200).send(user)
+    try {
+        const id = await req.params.id
+        // should ge user by given id in route param
+        const user = await users.findUser(id)
+        res.status(200)
+        res.send(user)
+    } catch (err) {
+        console.error(err);
+    }
+
 })
 
 app.delete('/user/:id', async (req, res) => {
-  const id = req.id
-  await users.deleteUser(id)
-  res.status(201).send({id})
+    try {
+        const id = req.params.id
+        await users.deleteUser(id)
+        console.log(id);
+        res.status(201)
+        res.send({ id })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 module.exports = app
--- exercises/testing/users.js
+++ exercises/testing/users.js
@@ -1,35 +1,50 @@
-const users = new Array(20).fill(0)
-.map((_, i) => {
-  return {
-    id: i,
-    createdAt: Date.now() + i,
-    email: `readycoder${i}@gmail.com`
-  }
-})
+const users = new Array(20)
+    .fill(0)
+    .map((_, i) => {
+        return {
+            id: i,
+            createdAt: Date.now() + i,
+            email: `readycoder${i}@gmail.com`
+        }
+    })
+
+const fixId = (id) => parseInt(id);
 
 // simulate async db call with promise
-const findUser = (id) => new Promise((resolve, reject) => {
-  const user = users.find(user => user.id === id)
-  if (user) {
-    return resolve(user)
-  }
-  reject(new Error(`No user with id "${id}"`))
-})
+const findUser = async (id) => {
+    try {
+        id = fixId(id);
+        const user = await users.find(user => user.id === id)
+        if (user) {
+
+            return user
+        }
+        new Error(`No user with id "${id}"`)
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // simulate async db call with promise
-const deleteUser = (id) => new Promise((resolve, reject) => {
-  const id = fixId(id)
-  const i = users.findIndex(user => user.id === id)
+const deleteUser = async (id) => {
+    try {
+        id = fixId(id);
+        const i = await users.findIndex(user => user.id === id)
 
-  if (i < 0) {
-    return reject(new Error(`No user with id "${id}"`))
-  }
+        if (i < 0) {
+            new Error(`No user with id "${id}"`)
+        }
 
-  users.slice(i, 1)
-  resolve({id})
-})
+        users.slice(i, 1)
+        return id;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 module.exports = {
-  findUser,
-  deleteUser
+    findUser,
+    deleteUser
 }
+
